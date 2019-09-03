b8ee6b9456a9cc9d638a762f944c323884622710
Moritz Schmidt <mo.schmidt@rebuy.com>
2019-09-03 20:07:14 +0200

    probably good recursive zsync create/update/delete/skip

--- src/service/ZSyncGenerationService.spec.ts
+++ src/service/ZSyncGenerationService.spec.ts
@@ -1,22 +1,46 @@
-import {ZSyncGenerationService} from './ZSyncGenerationService';
-import {writeFileSync, mkdirSync, readdirSync} from 'fs';
+import {UpdateResult, ZSyncGenerationService} from './ZSyncGenerationService';
+import {readdirSync, statSync} from 'fs';
+import {TODO} from '../util/funcs';
+import {ncp} from 'ncp';
+import {utimesSync} from 'fs';
+import {promisify} from 'util';
+
+const stoneAge = new Date('1981-10-11');
+const notOld = new Date('1996-12-30');
+
+
+async function prepareFixture(): Promise<string> {
+    const tmp = `/tmp/${Math.random()}`;
+    await promisify(ncp)(__dirname + '/../../resources/test/zsync-mock-fs', tmp);
+    utimesSync(`${tmp}/does-not-need-update`, stoneAge, stoneAge);
+    utimesSync(`${tmp}/does-not-need-update.zsync`, stoneAge, stoneAge);
+    utimesSync(`${tmp}/does-not-need-update2`, stoneAge, stoneAge);
+    utimesSync(`${tmp}/does-not-need-update2.zsync`, notOld, notOld);
+    utimesSync(`${tmp}/foo/missing`, stoneAge, stoneAge);
+    utimesSync(`${tmp}/foo/orphaned.zsync`, stoneAge, stoneAge);
+    utimesSync(`${tmp}/foo/bar/needs-update.zsync`, stoneAge, stoneAge);
+    utimesSync(`${tmp}/foo/bar/needs-update`, notOld, notOld);
+
+    return tmp;
+}
 
 describe(ZSyncGenerationService.name, () => {
 
     let args;
-    let service: ZSyncGenerationService;
-
     beforeEach(() => {
         args = [];
     });
 
-    describe('writeSingle', () => {
+    xdescribe('writeSingle', () => {
 
         it('executes correct cmdline', (done) => {
-            service = new ZSyncGenerationService(
+            let service = new ZSyncGenerationService(
                 'http://foo',
                 '/var/lib/foo',
-                function () {  args.push(arguments); return Promise.resolve({stdout: [], stderr: []}); },
+                function () {
+                    args.push(arguments);
+                    return Promise.resolve({stdout: [], stderr: []});
+                },
             );
             service.writeSingle('/@mod/x.pbo').then(() => {
                 expect(args).toHaveLength(1);
@@ -26,39 +50,82 @@ describe(ZSyncGenerationService.name, () => {
             });
         });
         it('fails when the command fails', (done) => {
-            service = new ZSyncGenerationService(
+            let service = new ZSyncGenerationService(
                 'http://foo',
                 '/var/lib/foo',
-                function () {  args.push(arguments); return Promise.reject({stdout: ['aaaah'], stderr: []}); },
+                function () {
+                    args.push(arguments);
+                    return Promise.reject({stdout: ['aaaah'], stderr: []});
+                },
             );
-            service.writeSingle('/@mod/x.pbo').then(() => { fail('bad!'); done();} ).catch((res) => {
+            service.writeSingle('/@mod/x.pbo').then(() => {
+                fail('bad!');
+                done();
+            }).catch((res) => {
                 expect(res.stdout.toString()).toEqual('aaaah');
                 done();
             });
         });
     });
-    describe('clearAll', () => {
+    xdescribe('clearAll', () => {
+
         it('recursively deletes everything named *.zsync', async (done) => {
-            let dir = `/tmp/node-arma3sync-lib-${Date.now()}`;
-            mkdirSync(dir);
-            mkdirSync(`${dir}/foo`);
-            mkdirSync(`${dir}/foo/bar`);
-            writeFileSync(`${dir}/a`, 'datafile');
-            writeFileSync(`${dir}/a.zsync`, 'datafile');
-            writeFileSync(`${dir}/foo/b.pbo`, 'datafile');
-            writeFileSync(`${dir}/foo/x.zsync`, 'datafile');
-            writeFileSync(`${dir}/foo/bar/xxx.sync`, 'datafile');
-            writeFileSync(`${dir}/foo/bar/.zsync`, 'datafile');
+            const dir = await prepareFixture();
 
-            const nDeleted = await service.clear(dir);
+            let service = new ZSyncGenerationService(
+                'http://foo',
+                dir,
+                function () {
+                    args.push(arguments);
+                    return Promise.resolve({stdout: [], stderr: []});
+                },
+            );
 
-            expect(nDeleted).toBe(3);
+            const nDeleted = await service.clear('/');
 
-            expect(readdirSync(dir)).toEqual(['a', 'foo']);
-            expect(readdirSync(`${dir}/foo`)).toEqual(['b.pbo', 'bar']);
-            expect(readdirSync(`${dir}/foo/bar`)).toEqual(['xxx.sync']);
+            expect(nDeleted).toBe(4);
+
+            expect(readdirSync(`${dir}/`)).toEqual(['does-not-need-update', 'does-not-need-update2', 'foo']);
+            expect(readdirSync(`${dir}/foo`)).toEqual(['bar', 'missing']);
+            expect(readdirSync(`${dir}/foo/bar`)).toEqual(['needs-update']);
 
             done();
+        });
+    });
+
+    describe('update', () => {
+
+        let service;
+        let dir;
+        beforeEach(async (done) => {
+            dir = await prepareFixture();
+            service = new ZSyncGenerationService(
+                'http://foo',
+                dir,
+                function () {
+                    args.push(arguments);
+                    return Promise.resolve({stdout: [], stderr: []});
+                },
+            );
+            done();
+        });
+
+        it('returns good create/update/delete/skip counts', async (done) => {
+            const counts = await service.update('');
+            expect(counts).toEqual(new UpdateResult(1, 1, 1, 2));
+            done();
+        });
+
+        it('updates files older than certain date', async (done) => {
+            await service.update('');
+            expect(args[0]).toContain(`zsyncmake -eu http://foo/foo/bar/needs-update ${dir}/foo/bar/needs-update -o ${dir}/foo/bar/needs-update.zsync`);
+            done();
+        });
+        it('generates missing files', async (done) => {
+            await service.update('');
+            expect(args[1]).toContain(`zsyncmake -eu http://foo/foo/missing ${dir}/foo/missing -o ${dir}/foo/missing.zsync`);
+            done();
+
         });
     });
 });
--- src/service/ZSyncGenerationService.ts
+++ src/service/ZSyncGenerationService.ts
@@ -1,12 +1,57 @@
 import {exec} from 'child_process';
 import {promisify} from 'util';
 import {Path} from '../util/aliases';
-import {TODO} from '../util/funcs';
-import {readdir, unlink} from 'fs';
+import {Dirent, readdir, stat, unlink} from 'fs';
 
 function sum(numbers: number[]): number {
     return numbers.reduce((a, b) => a + b, 0)
 }
+
+export class UpdateResult {
+    constructor(
+    public readonly created: number = 0,
+    public readonly updated: number = 0,
+    public readonly deleted: number = 0,
+    public readonly skipped: number = 0
+) {}
+    public add(other: UpdateResult): UpdateResult {
+        return new UpdateResult(
+            this.created + other.created,
+            this.updated + other.updated,
+            this.deleted + other.deleted,
+            this.skipped + other.skipped,
+        );
+    }
+
+    public equals(other: UpdateResult): boolean {
+        return this.created === other.created && this.updated === other.updated && this.deleted === other.deleted && this.skipped === other.skipped;
+    }
+
+    public static Created = new UpdateResult(1, 0, 0, 0);
+    public static Updated = new UpdateResult(0, 1, 0, 0);
+    public static Deleted = new UpdateResult(0, 0, 1, 0);
+    public static Skipped = new UpdateResult(0, 0, 0, 1);
+
+    public static sum(updateResults: UpdateResult[]): UpdateResult {
+        return updateResults.reduce((prev, cur) => {
+            return prev.add(cur);
+        }, new UpdateResult());
+    }
+}
+
+/**
+ * removes value from array, returns removed element (if any)
+ */
+function consume<T>(arr: T[], val: T): T|undefined {
+    const idx = arr.indexOf(val);
+    if (idx === -1) {
+        return;
+    }
+
+    return arr.splice(idx, 1)[0];
+}
+
+const zSyncFilenameExtension = '.zsync';
 
 /**
  * because we don't have a working zsync implementation in the JS world,
@@ -39,19 +84,36 @@ export class ZSyncGenerationService {
         }
     }
 
+    private async writeRelativeSingle(relativePath: Path, filename: string): Promise<void> {
+        return this.writeSingle(`${relativePath}/${filename}`);
+    }
+
+    private getMtimeMs(relativePath: Path, filename: string): Promise<number> {
+        return promisify(stat)(`${this.repoPath}${relativePath}/${filename}`)
+            .then(stat => stat.mtimeMs);
+    }
+
+    private async deleteZsync(relativePath: Path, filename: string): Promise<void> {
+        return promisify(unlink)(`${this.repoPath}${relativePath}/${filename}`);
+    }
+
     /**
      * removes all zsync files from path
      * returns number of deleted files
      */
     public async clear(path: Path): Promise<number> {
-        const entries = await promisify(readdir)(path, {withFileTypes: true});
+        if (!this.repoPath || this.repoPath === '/') {
+            throw new Error('repo path is filesystem root. this should not be.');
+        }
+        const absolutePath = this.repoPath + path;
+        const entries = await promisify(readdir)(absolutePath, {withFileTypes: true});
         const zsyncs: string[] = entries.filter(dirent => dirent.isFile() && dirent.name.endsWith('.zsync')).map(dirent => dirent.name);
         const subdirs: string[] = entries.filter(dirent => dirent.isDirectory() && dirent.name !== '.' && dirent.name !== '..').map(dirent => dirent.name);
         const recursingPromises: number[] = await Promise.all(subdirs.map(subdir => {
             return this.clear(`${path}/${subdir}`);
         }));
         await Promise.all(zsyncs.map(zsync => {
-            return promisify(unlink)(`${path}/${zsync}`);
+            return this.deleteZsync(path, zsync);
         }));
 
         return Promise.resolve(zsyncs.length + sum(recursingPromises));
@@ -61,8 +123,61 @@ export class ZSyncGenerationService {
      * update all zsyncs if they're older than the files they refer to,
      * or create if not existent
      */
-    public async update(path: Path): Promise<void> {
-        return Promise.reject(TODO());
+    public async update(relativePath: Path): Promise<UpdateResult> {
+        if (!this.repoPath || this.repoPath === '/') {
+            throw new Error('repo path is filesystem root. this should not be.');
+        }
+
+        return (await this.updateRecursion(relativePath)).add(await this.updateSingleDirectory(relativePath));
+    }
+
+    private async updateRecursion(relativePath: Path): Promise<UpdateResult> {
+        const absolutePath = this.repoPath + relativePath;
+        const entries: Dirent[] = await promisify(readdir)(absolutePath, {withFileTypes: true});
+        const subdirs: string[] = entries.filter(dirent => dirent.isDirectory() && dirent.name !== '.' && dirent.name !== '..').map(dirent => dirent.name);
+        return UpdateResult.sum(await Promise.all(subdirs.map(subdir => {
+            return this.update(`${relativePath}/${subdir}`);
+        })));
+    }
+
+    private async updateSingleDirectory(relativePath: Path): Promise<UpdateResult> {
+        const absolutePath = this.repoPath + relativePath;
+        const entries: Dirent[] = await promisify(readdir)(absolutePath, {withFileTypes: true});
+        const files = entries
+            .filter(dirent => dirent.isFile())
+            .map(dirent => dirent.name);
+
+        const zsyncs: string[] = files.filter(path => path.endsWith(zSyncFilenameExtension));
+        const nonZsyncs: string[] = files.filter(path => !path.endsWith(zSyncFilenameExtension));
+        let result = await Promise
+            .all(nonZsyncs.map(nonZsync => this.updateForFile(relativePath, nonZsync, zsyncs)))
+            .then(results => UpdateResult.sum(results));
+        result = result.add(await this.deleteZsyncs(relativePath, zsyncs));
+
+        return result;
+    }
+
+    private async deleteZsyncs(relativePath: Path, zsyncs: string[]): Promise<UpdateResult> {
+        return Promise.all(zsyncs.map(zsync => {
+            return this.deleteZsync(relativePath, zsync).then(() => UpdateResult.Deleted);
+        })).then(results => UpdateResult.sum(results))
+    }
+
+    private async updateForFile(relativePath: Path, nonZsync: string, zsyncs: string[]): Promise<UpdateResult> {
+        const zsyncPath = nonZsync + zSyncFilenameExtension;
+        if (consume(zsyncs, zsyncPath)) {
+            return Promise.all([
+                this.getMtimeMs(relativePath, nonZsync),
+                this.getMtimeMs(relativePath, zsyncPath),
+            ]).then(mtimeMs => {
+                if (mtimeMs[0] > mtimeMs[1]) {
+                    return this.writeRelativeSingle(relativePath, nonZsync).then(() => UpdateResult.Updated);
+                }
+                return Promise.resolve(UpdateResult.Skipped);
+            });
+        }
+        return this.writeRelativeSingle(relativePath, nonZsync).then(() => UpdateResult.Created);
+
     }
 
     private static checkPath(file: Path) {
@@ -73,7 +188,7 @@ export class ZSyncGenerationService {
 
     /**
      *
-     * @param file Path  absolute path within repo, e.g. /@mod/addons/foo.pbo
+     * @param file Path path within repo, e.g. /@mod/addons/foo.pbo
      */
     private getCmdLine(file: Path): string {
         return `zsyncmake -eu ${this.repoPublicURL}${file} ${this.repoPath}${file} -o ${this.repoPath}${file}.zsync`;
